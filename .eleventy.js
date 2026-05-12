fd9a5bdb0cea688e053142a97c4c288c6fec5156
aaronxu <xuaaronj@gmail.com>
2026-05-12 16:22:30 -0500

    fixed passthroughcopy

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,7 @@
 module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy("src/assets");
-  eleventyConfig.addPassthroughCopy("src/modules/figures");
+  eleventyConfig.addPassthroughCopy("src/figures");
+  eleventyConfig.addPassthroughCopy("src/applet");
 
   return {
     dir: {
